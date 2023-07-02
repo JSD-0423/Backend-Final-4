4e98615f31a93deff94afd755a44b474f0f3bfd5
mohanadft <mohanadfteha@gmail.com>
2023-07-02 19:41:31 +0300

    Add build folder

---
+++ build/config/env.config.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    port: process.env.PORT,
+    dbHost: process.env.DB_HOST,
+    dbUser: process.env.DB_USER,
+    dbName: process.env.DB_NAME
+};
+//# sourceMappingURL=env.config.js.map
