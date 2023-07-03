4d64c201f043f2aece7157f6e2030681c95e6812
mohanadft <mohanadfteha@gmail.com>
2023-07-03 19:26:39 +0300

    :construction_worker: Build

---
+++ build/config/env.config.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    port: process.env.PORT,
+    dbHost: process.env.DB_HOST,
+    dbUser: process.env.DB_USER,
+    dbName: process.env.DB_NAME,
+    dbPort: process.env.DB_PORT,
+    dbPassword: process.env.DB_PASSWORD,
+};
+//# sourceMappingURL=env.config.js.map
