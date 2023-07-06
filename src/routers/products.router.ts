97b515c0f7aba1e5a26e7beed41b571ee17ba3a9
mohanadft <mohanadfteha@gmail.com>
2023-07-06 03:06:26 +0300

    Successfully tested cloudinary uploading images

--- src/routers/products.router.ts
+++ src/routers/products.router.ts
@@ -1,11 +1,8 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import { getProducts } from '../controllers/products.controller';
 
 const productsRouter: Router = Router();
 
 productsRouter.get('/', getProducts);
-productsRouter.post('/', (req: Request, res: Response) => {
-  console.log(req);
-});
 
 export default productsRouter;
