86fab886912c528589e510ac9c691cdbcfd1b2e8
mohanadft <mohanadfteha@gmail.com>
2023-07-08 19:08:48 +0300

    :sparkles: Make some refactoring and Implement `getProduct` controller

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,6 +1,12 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { Product, Review } from '../models';
 import { Sequelize } from 'sequelize';
+import { CustomError } from '../middlewares/errors';
+import httpStatus from 'http-status';
+
+export interface Params {
+  id: number
+}
 
 
 const getProducts: RequestHandler = async (
@@ -9,24 +15,17 @@ const getProducts: RequestHandler = async (
   next: NextFunction,
 ) => {
   try {
-    const products = await Product.findAll(
-      {
-        include: {
-          model: Review,
-          attributes: [],
-        },
-        attributes: [
-          'id',
-          'name',
-          'description',
-          'color',
-          'price',
-          'discount',
-          [Sequelize.fn('AVG', Sequelize.col('rating')), 'ratings']
-        ],
-        group: ['Product.id']
+    const products = await Product.findAll({
+      include: {
+        model: Review,
+        attributes: [],
       },
-    );
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+        include: [[Sequelize.literal('(SELECT AVG(`rating`) FROM `reviews` WHERE `reviews`.`product_id` = `Product`.`id`)'), 'rating'],]
+      }
+    });
+
     res.json(products);
   } catch(e) {
     console.log(e);
@@ -34,4 +33,31 @@ const getProducts: RequestHandler = async (
   }
 };
 
-export { getProducts };
+const getProduct: RequestHandler<Params> =async (
+  req: Request<Params>,
+  res: Response<Product>,
+  next:NextFunction
+) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findByPk(id, {
+      include: {
+        model: Review,
+        attributes: [],
+      },
+      attributes: {
+        exclude: ['createdAt', 'updatedAt'],
+        include: [[Sequelize.literal('(SELECT AVG(`rating`) FROM `reviews` WHERE `reviews`.`product_id` = `Product`.`id`)'), 'rating'],]
+      }
+    });
+
+    if(!product) throw new CustomError('Product not found', httpStatus.NOT_FOUND);
+
+    res.json(product);
+  } catch(e) {
+    next(e);
+  }
+};
+
+export { getProducts, getProduct };
