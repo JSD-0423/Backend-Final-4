b58a2f19efbf9d28f6640c57ca69dd24bbd6ee78
mohanadft <mohanadfteha@gmail.com>
2023-07-13 19:44:08 +0300

    :building_construction: Setup product validator

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -12,11 +12,12 @@ export const errorHandler = (
     return res.status(error.statusCode).json({ error: error.message });
   }
 
-
   if (error instanceof CustomValidationError) {
     const { errors, statusCode } = error;
-    return res.status(statusCode).json({ errors: errors.map((e) => e.message) });
+    return res.status(statusCode).json({ errors: errors.map(e => e.message) });
   }
 
-  res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: 'Internal Server Error' });
+  res
+    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+    .json({ error: 'Internal Server Error' });
 };
---
+++ src/validators/product.validator.ts
@@ -0,0 +1,29 @@
+import Joi from 'joi';
+import { CustomValidationError } from '../middlewares/errors';
+
+export interface Product {
+  name: string;
+  description: string;
+  color: string;
+  price: number;
+  discount: number;
+  rating: number;
+}
+
+const ProductSchema = Joi.object({
+  name: Joi.string().alphanum().required(),
+  descritpion: Joi.string().required(),
+  color: Joi.string().required(),
+  price: Joi.number().required(),
+  discount: Joi.number().optional().default(0),
+  rating: Joi.number().default(0)
+});
+
+const validateProduct = (body: Product) => {
+  const { error } = ProductSchema.validate(body);
+  if (error) {
+    throw new CustomValidationError('Invalidated Fields', error.details);
+  }
+};
+
+export default validateProduct;
