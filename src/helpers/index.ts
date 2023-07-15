829a2e716e7e127c49f558527d730dda4b4adff6
mohanadft <mohanadfteha@gmail.com>
2023-07-15 04:40:13 +0300

    :sparkles: Add `helpers` folder

---
+++ src/helpers/index.ts
@@ -0,0 +1,6 @@
+import { NextFunction, Request, RequestHandler, Response } from 'express';
+
+const use =
+  <P, ResBody, ReqBody>(fn: RequestHandler<P, ResBody, ReqBody>) =>
+  (req: Request<P, ResBody, ReqBody>, res: Response, next: NextFunction) =>
+    Promise.resolve(fn(req, res, next)).catch(next);
