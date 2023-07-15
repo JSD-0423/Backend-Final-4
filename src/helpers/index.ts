44148a7a944075de242c3ab5f257506cb549f8aa
mohanadft <mohanadfteha@gmail.com>
2023-07-15 05:54:14 +0300

    :recycle: Add more generice types to `use` function

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,8 +1,14 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 
 const use =
-  <P, ResBody, ReqBody>(fn: RequestHandler<P, ResBody, ReqBody>) =>
-  (req: Request<P, ResBody, ReqBody>, res: Response, next: NextFunction) =>
+  <P, ResBody, ReqBody, Query>(
+    fn: RequestHandler<P, ResBody, ReqBody, Query>
+  ) =>
+  (
+    req: Request<P, ResBody, ReqBody, Query>,
+    res: Response,
+    next: NextFunction
+  ) =>
     Promise.resolve(fn(req, res, next)).catch(next);
 
 export { use };
