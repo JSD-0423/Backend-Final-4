af99f48e9a15ef16afc44efb91b6d5993efbb408
mohanadft <mohanadfteha@gmail.com>
2023-07-26 18:13:28 +0300

    :monocle_face: Enhance sign in validation

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Request, RequestHandler, Response } from 'express';
 import { User } from '../models';
-import { validateUser } from '../validators';
+import { validateSignedInUsers, validateUser } from '../validators';
 import { Payload } from '../interfaces';
 import httpStatus from 'http-status';
 import { CustomError } from '../middlewares/errors';
 import { compare } from '../utils/bcrypt';
 import { generateToken } from '../helpers';
 import envConfig from '../config/env.config';
+import { SignIn } from '../validators/signIn.validator';
 
 const signUp: RequestHandler<object, object, User> = async (
   req: Request<object, object, User>,
@@ -26,8 +27,11 @@ const signUp: RequestHandler<object, object, User> = async (
   res.status(httpStatus.CREATED).json(payload);
 };
 
-const signIn: RequestHandler = async (req: Request, res: Response) => {
-  validateUser(req.body);
+const signIn: RequestHandler<object, object, SignIn> = async (
+  req: Request<object, object, SignIn>,
+  res: Response
+) => {
+  validateSignedInUsers(req.body);
   const { email, password } = req.body;
 
   const user = await User.findOne({ where: { email } });
--- src/validators/index.ts
+++ src/validators/index.ts
@@ -3,3 +3,4 @@ export { default as validateCategory } from './category.validator';
 export { default as validateBrand } from './brand.validator';
 export { default as validateUser } from './user.validator';
 export { default as validateAddress } from './address.validator';
+export { default as validateSignedInUsers } from './signIn.validator';
