91627fa6149f1ada1f8a230ca857b0163f371b9c
mohanadft <mohanadfteha@gmail.com>
2023-07-26 14:12:53 +0300

    :children_crossing: Throw an error if the token not exists

--- src/middlewares/setUserId.middleware.ts
+++ src/middlewares/setUserId.middleware.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 import jwt from 'jsonwebtoken';
+import { CustomError } from './errors';
+import { UNAUTHORIZED } from 'http-status';
 
 declare global {
   namespace Express {
@@ -15,6 +17,7 @@ const setUserId: RequestHandler = (
   next: NextFunction
 ) => {
   const { token } = req.cookies;
+  if (!token) next(new CustomError('Unauthenticated', UNAUTHORIZED));
   const user = jwt.decode(token) as { id: number };
   req.userId = user.id;
   next();
