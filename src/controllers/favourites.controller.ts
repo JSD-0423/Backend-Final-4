c4066069cc87bb9d8d45c14a7eb70c1dbf407f08
mohanadft <mohanadfteha@gmail.com>
2023-07-28 21:32:25 +0300

    :sparkles: Implement `toggle` function
    to remove or add products

---
+++ src/controllers/favourites.controller.ts
@@ -0,0 +1,34 @@
+import { Request, RequestHandler, Response } from 'express';
+import { FavouriteList, Product, User } from '../models';
+import { CustomError } from '../middlewares/errors';
+import httpStatus from 'http-status';
+
+const toggleFav: RequestHandler<object, object, { productId: number }> = async (
+  req: Request<object, object, { productId: number }>,
+  res: Response
+) => {
+  const { productId } = req.body;
+
+  const userId = req.userId;
+
+  const user = await User.findByPk(userId);
+  const product = await Product.findByPk(productId);
+
+  if (!user) throw new CustomError('User not found', httpStatus.NOT_FOUND);
+  if (!product)
+    throw new CustomError('Product not found', httpStatus.NOT_FOUND);
+
+  const isProductInFavorites = await user.$has('favouriteList', product);
+
+  if (isProductInFavorites) {
+    await user.$remove('favouriteList', product);
+    res
+      .status(httpStatus.OK)
+      .json({ message: 'Product removed from favorites' });
+  } else {
+    await user.$add('favouriteList', product);
+    res.status(httpStatus.OK).json({ message: 'Product added to favorites' });
+  }
+};
+
+export { toggleFav };
